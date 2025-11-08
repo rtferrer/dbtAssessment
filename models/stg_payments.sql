923527b0ed06d698fa9223840c7f977963be112f
Rache Ferrer <rtanferrer@gmail.com>
2025-11-08 11:41:08 +0000

    Added new report rpt_daily_revenue.sql

---
+++ models/stg_payments.sql
@@ -0,0 +1,9 @@
+select
+    id                     as payment_id,
+    orderid                as order_id,
+    paymentmethod,
+    status                 as payment_status,
+    amount                 as payment_amount,
+    created::date          as payment_date,
+
+from {{ source('stripe', 'payment') }}
